4f95681af07edcae8883ea78c6286b28abe14dc4
Curso <Curso@IPARTEKAULA.COM>
2018-03-12 14:31:12 +0100

    una prueba

--- src/app/providers/servicio.service.ts
+++ src/app/providers/servicio.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 const END_POINT = "192.168.99.1:3000/todos";
 @Injectable()
 export class ServicioService {
@@ -7,3 +8,11 @@ export class ServicioService {
   constructor() { }
 
 }
+/*
+getTodos():Observable<any>{
+  let url = END_POINT + '/todos';
+  console.log(`TodosService getTodos ${url}`);
+
+  return this.http.get(url);
+
+}*/
